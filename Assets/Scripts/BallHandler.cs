8813e4b75973e2b89bb07d8b79ea142e1fa2a51f
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-10-07 08:43:54 +0300

    - Added iTween.cs; - Added Circle/Circle1.cs;

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BallHandler : MonoBehaviour
 {
+    public static float rotationSpeed = 100;
+
     public static Color oneColor = Color.green;
     public GameObject ball;
 
@@ -12,7 +12,7 @@ public class BallHandler : MonoBehaviour
 
     void Start()
     {
-        
+        MakeANewCircle();
     }
 
     
@@ -29,6 +29,13 @@ public class BallHandler : MonoBehaviour
         GameObject clone = Instantiate<GameObject>(ball, new Vector3(0, 0, -8), Quaternion.identity); // By using Generics we don't need to cast the result to a specific type;
         clone.GetComponent<MeshRenderer>().material.color = oneColor;
         clone.GetComponent<Rigidbody>().AddForce(Vector3.forward * _speed, ForceMode.Impulse); // Time does not affect on ForceMode.Impulse;
+    }
+
+    void MakeANewCircle()
+    {
+        GameObject gameObject2 = Instantiate(Resources.Load("Round" + Random.Range(1, 4))) as GameObject;
+        gameObject2.transform.position = new Vector3(0, 20, 23);
+        gameObject2.name = "Circle";
     }
 
 
