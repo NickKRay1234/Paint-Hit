19fad1c54ece50e8a984837bdfc11175ac672364
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-10-08 17:24:11 +0300

    - Added some comments in Color Scripts/ColorChanger.cs;

--- Assets/Scripts/Color Scripts/ColorChanger.cs
+++ Assets/Scripts/Color Scripts/ColorChanger.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
+    [SerializeField] private GameObject _splash;
+
+
     void OnCollisionEnter(Collision target)
     {
         if(target.gameObject.tag == "red")
         {
-
+            base.gameObject.GetComponent<Collider>().enabled = false;
+            target.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            target.gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+            base.GetComponent<Rigidbody>().AddForce(Vector3.down * 50, ForceMode.Impulse);
+            target.gameObject.name = "Is colored by Red";
+            Destroy(base.gameObject, .5f);
+            print("GameOver");
         }
         else
         {
-            base.gameObject.GetComponent<Collider>().enabled = true;
-            GameObject gameObject = Instantiate(Resources.Load("splash1")) as GameObject;
-            gameObject.transform.parent = target.gameObject.transform;
-            Destroy(gameObject, 0.1f);
-            target.gameObject.name = "color";
+            base.gameObject.GetComponent<Collider>().enabled = false; // Ball is unvisible
+
+            // Creating and destroying a splash
+            GameObject SampleSplash = Instantiate(_splash) as GameObject;
+            SampleSplash.transform.parent = target.gameObject.transform;
+            Destroy(SampleSplash, 0.1f);
+
+            //Information
+            target.gameObject.name = "Is colored by Green";
             target.gameObject.tag = "red";
             StartCoroutine(ChangeColor(target.gameObject));
         }
