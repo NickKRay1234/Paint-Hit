d76be086bae045c9cef8d86b234d6a5065df8ca2
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-10-11 08:27:25 +0300

    - Added making a new circles;

--- Assets/Scripts/Handler Scripts/BallHandler.cs
+++ Assets/Scripts/Handler Scripts/BallHandler.cs
@@ -15,10 +15,20 @@ public class BallHandler : MonoBehaviour
     public static float rotationSpeed = 100;
     private float _speed = 100;
 
+    //new
+    private int _ballsCount;
+    private int _circleNumber;
+
 
     private void Start()
     {
+        GameObject circle = Instantiate(_circles[Random.Range(0, 4)]);
+        circle.transform.position = new Vector3(0, 20, 23);
+        circle.name = "Circle" + _circleNumber;
+
         MakeANewCircle();
+        //new 
+        _ballsCount = 4;
     }
 
     
@@ -30,6 +40,15 @@ public class BallHandler : MonoBehaviour
 
     private void Fire()
     {
+        //new
+        if(_ballsCount <= 1)
+        {
+            base.Invoke("MakeANewCircle", 0.4f);
+            // Disable Button for some time;
+        }
+
+        _ballsCount--;
+
         GameObject clone = Instantiate<GameObject>(_ball, _dummyBall.transform.position, Quaternion.identity); // By using Generics we don't need to cast the result to a specific type;
         clone.GetComponent<MeshRenderer>().material.color = Color.green;
         clone.GetComponent<Rigidbody>().AddForce(Vector3.forward * _speed, ForceMode.Impulse); // Time does not affect on ForceMode.Impulse;
@@ -37,9 +56,26 @@ public class BallHandler : MonoBehaviour
 
     private void MakeANewCircle()
     {
+        //new
+        GameObject[] array = GameObject.FindGameObjectsWithTag("circle");
+        GameObject gameObject = GameObject.Find("Circle" + _circleNumber);
+        for(int i = 0; i < 24; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        gameObject.transform.GetChild(24).gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+
+        if(gameObject.GetComponent<iTween>())
+            gameObject.GetComponent<iTween>().enabled = false;
+
+        foreach(GameObject target in array)
+            iTween.MoveBy(target, iTween.Hash(new object[] { "y", -2.98f, "easeType", iTween.EaseType.spring, "time", 0.5 }));
+        _circleNumber++;
+
+
         GameObject circle = Instantiate(_circles[Random.Range(0, 4)]);
         circle.transform.position = new Vector3(0, 20, 23);
-        circle.name = "Circle";
+        circle.name = "Circle" + _circleNumber;
     }
 
 
